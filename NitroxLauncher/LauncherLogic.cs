ba82395e4efc3888a10686eae0f4729c19b8f878
zortofficial <76618862+zortofficial@users.noreply.github.com>
2020-12-26 16:26:29 +0300

    Update LauncherLogic.cs

--- NitroxLauncher/LauncherLogic.cs
+++ NitroxLauncher/LauncherLogic.cs
@@ -298,7 +298,7 @@ namespace NitroxLauncher
             }
             else if (PlatformDetection.IsSteam(subnauticaPath))
             {
-                startInfo.FileName = "steam://run/264710";
+                startInfo.FileName = "C:\Program Files (x86)\Steam\steamapps\common\Subnautica";
             }
             else if (PlatformDetection.IsMicrosoftStore(subnauticaPath))
             {
